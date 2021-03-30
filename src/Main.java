a78534c9fc3d380f66396e40e8bb6a9ecf259fae
alessandro <alethecine96@gmail.com>
2021-03-30 20:17:03 +0200

    remove TODO

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.io.FileNotFoundException;
 
-public class Main {  //TODO Aggiungere tempo di volo perchè si accavallano sennò sia in partenza che in arrivo
+public class Main {
     //TODO inserire nei messaggi il tipo di aereo utilizzato dal volo
 
     public static void main(String[] args) throws FileNotFoundException {
