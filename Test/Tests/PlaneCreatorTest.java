795639855f246f8390f70be44b1f286a738808be
alessandro <alethecine96@gmail.com>
2021-04-06 16:07:02 +0200

    changes

--- Test/Tests/PlaneCreatorTest.java
+++ Test/Tests/PlaneCreatorTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PlaneBuilderTest {
+public class PlaneCreatorTest {
     private PlaneCreator testplaneCreator;
 
     @BeforeEach
