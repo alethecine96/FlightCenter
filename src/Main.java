3931c7fc591036b0736d3eddf3ca63b8f7496500
alessandro <alethecine96@gmail.com>
2021-04-07 13:01:40 +0200

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.io.FileNotFoundException;
 
 public class Main {
-    //TODO inserire nei messaggi il tipo di aereo utilizzato dal volo
 
     public static void main(String[] args) throws FileNotFoundException {
         FlightManager flightManager = new FlightManager();
